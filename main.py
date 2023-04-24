933111ca21cc441c78a074954453034a6a677237
maxfahl <max@fahl.se>
2023-04-24 10:23:44 +0200

    Small improvements to the prompt.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if not OPENAI_API_KEY:
 openai.api_key = OPENAI_API_KEY
 
 DEBUG = config('DEBUG') == 'true'
-ANSWER_TEMPLATE = '{"topic": "<TOPIC>", "answer": "<YOUR_ANSWER>", "follow_up_questions": ["<follow_up_question_1>", "<follow_up_question_2>", "<follow_up_question_3>", "<follow_up_question_4>", "<follow_up_question_5>"]}'
+ANSWER_TEMPLATE = '{"answer": "<YOUR_ANSWER>", "topic": "<TOPIC>", "follow_up_questions": ["<follow_up_question_1>", "<follow_up_question_2>", "<follow_up_question_3>", "<follow_up_question_4>", "<follow_up_question_5>", "<follow_up_question_6>"]}'
 MAX_HISTORY_TOKENS = 3000
 
 encoder = tiktoken.encoding_for_model('gpt-3.5-turbo')
@@ -44,7 +44,7 @@ def has_qa_history():
 
 
 def get_qa_history_formatted():
-    return '\n\n'.join(qa_history)
+    return '\n'.join(qa_history)
 
 
 def previous_qa_history_length_ok():
@@ -60,6 +60,7 @@ def trim_qas():
 
 
 def do_request(prompt):
+    debug_log(f'Prompt:\n\n{prompt}');
     print_magenta("\nThinking...")
     response = openai.ChatCompletion.create(
         messages=[{"role": "system", "content": prompt}],
@@ -67,37 +68,36 @@ def do_request(prompt):
         max_tokens=600,
         n=1,
         stop=None,
-        temperature=0.25,
+        temperature=0.5,
     )
     return response.choices[0].message.content.strip()
 
 
 def ask(question, topic=None):
     parts = []
-    parts.append('--- START OF CONTEXTUAL INFORMATION')
+    parts.append('--- START OF CONTEXT')
     if has_qa_history():
         parts.append(f'[Q&A HISTORY]\n{get_qa_history_formatted()}')
     if topic:
         parts.append(f'[TOPIC]\n{topic}')
     parts.append(f'[CURRENT QUESTION]\n{question}')
-    parts.append('--- END OF CONTEXTUAL INFORMATION')
+    parts.append('--- END OF CONTEXT')
     parts.append('--- START OF PROMPT')
     directives = []
     directives.append(
         'You are an AI researcher that answers questions factually correct, the answers should lead to curiosity. Elaborate on the answers as much as possible. Here are your directives:')
     directives.append('1. Answer the [CURRENT QUESTION].')
     if topic:
-        parts.append(f'2. Come up with a topic that suits the latest 2 questions of [Q&A HISTORY], the topic should be short but descriptive.')
+        directives.append(f'2. Come up with a topic that suits the latest 3 questions of [Q&A HISTORY], the topic should be short but descriptive.')
     directives.append(
-        f'{"3" if topic else "2"}. Create 5 follow-up questions for your current answer.{" Try and stay as close to the [TOPIC] as possible." if topic else ""}')
+        f'{"3" if topic else "2"}. Create 5 follow-up questions for your current answer.{" Try and stay as close to the [TOPIC] as possible." if topic else ""}. The 6th follow-up question should derive from the [TOPIC], by still being related to the [CURRENT QUESTION].')
     parts.append('\n'.join(directives))
     if len(qa_history):
         parts.append(
             'Analyze the [Q&A HISTORY] to decrease repetition of answers and follow-up questions.')
+
     parts.append(
-        'Try to keep the answers relevant for the [TOPIC] as possible, and do not diverge from it as long as the user\'s questions does not indicate a clear change of interest.')
-    parts.append(
-        f'IMPORTANT: Only respond in JSON formatting following the following template exactly:\n\n{ANSWER_TEMPLATE}')
+        f'IMPORTANT: Only respond in JSON formatting using the following template exactly:\n\n{ANSWER_TEMPLATE}')
     prompt = '\n\n'.join(parts)
 
     response = do_request(prompt)
@@ -105,8 +105,9 @@ def ask(question, topic=None):
     success = False
     data = None
     answer = None
-    options = None
     topic = None
+    options = None
+
     try:
         data = json.loads(response)
     except json.JSONDecodeError as e:
@@ -119,23 +120,25 @@ def ask(question, topic=None):
 
     if answer:
         print_green(f"\n{answer}")
-        qa_history.append(f'[QUESTION]: {question}\n[ANSWER]: {answer}')
+        qa_history.append(f'Question: {question}\nAnswer: {answer}')
         trim_qas()
         success = True
 
     if options:
         options = list(map(lambda x: x.strip(), options))
 
+    debug_log(f'Topic: {topic if topic else "None"}')
+
     if DEBUG and not answer or not topic or not options:
         print_yellow(f'Could not parse response answer, topic or options from: {response}')
 
-    return success, options, topic
+    return success, topic, options
 
 
 def main():
     while True:
         original_question = input("\nEnter a question:\n")
-        success, options, topic = ask(original_question)
+        success, topic, options = ask(original_question)
         topic = topic if topic else original_question
 
         while True:
@@ -151,14 +154,14 @@ def main():
                 user_input = input(
                     '\nChoose a follow-up question by entering the corresponding number (1-5) or type a custom question:\n')
 
-                if user_input.isdigit() and 1 <= int(user_input) <= 5:
+                if user_input.isdigit() and 1 <= int(user_input) <= 6:
                     follow_up_question = options[int(user_input) - 1]
                 else:
                     follow_up_question = user_input
             else:
                 follow_up_question = input('\nCould not find any follow-up questions, please provide one yourself:\n')
 
-            success, options, updated_topic = ask(follow_up_question, topic)
+            success, updated_topic, options = ask(follow_up_question, topic)
             topic = updated_topic if updated_topic else topic
 
 
