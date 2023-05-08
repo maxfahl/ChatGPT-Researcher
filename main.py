9ece7b602599e7efb5e01857dc1d90004c9f944a
maxfahl <max@fahl.se>
2023-05-08 08:47:34 +0200

    Count tokens and pop messages when too long.

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
-from decouple import config
-import openai
+import sys
+import os
 import json
+import openai
 import tiktoken
+from dotenv import load_dotenv
+from datetime import datetime
 from colorama import init
-from termcolor import  cprint
-
+from termcolor import cprint
 
 init()
+load_dotenv()
 
-OPENAI_API_KEY = config('OPENAI_API_KEY')
-
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 if not OPENAI_API_KEY:
     print('OpenAI API key not defined in .env.')
     quit()
+
+MODEL = os.getenv('MODEL', default="gpt-3.5-turbo")
+MAX_TOKENS = int(os.getenv('MAX_TOKENS', default='4000'))
+DEBUG = os.getenv('DEBUG', default='false') == 'true'
+ANSWER_TEMPLATE = '{"answer": "<YOUR_ANSWER>", "topic": "<TOPIC>", "follow_up_questions": ["<follow_up_question_1>", "<follow_up_question_2>", "<follow_up_question_3>", "<follow_up_question_4>", "<follow_up_question_5>", "<follow_up_question_6>"]}'
+
 openai.api_key = OPENAI_API_KEY
 
-MODEL = config('MODEL', default="gpt-3.5-turbo")
-DEBUG = config('DEBUG', default='false') == 'true'
-ANSWER_TEMPLATE = '{"answer": "<YOUR_ANSWER>", "topic": "<TOPIC>", "follow_up_questions": ["<follow_up_question_1>", "<follow_up_question_2>", "<follow_up_question_3>", "<follow_up_question_4>", "<follow_up_question_5>", "<follow_up_question_6>"]}'
-MAX_HISTORY_TOKENS = 3000
-
-encoder = tiktoken.encoding_for_model('gpt-3.5-turbo')
-qa_history = []
-
 print_green = lambda text: cprint(text, "green")
-print_blue = lambda text: cprint(text, "blue")
 print_magenta = lambda text: cprint(text, "magenta")
 print_cyan = lambda text: cprint(text, "cyan")
 print_yellow = lambda text: cprint(text, "yellow")
@@ -35,31 +34,60 @@ def debug_log(text):
         print_yellow(text)
 
 
-def get_num_tokens(text):
-    return len(encoder.encode(text))
+def save_conversation_history(conversation):
+    history_dir = "history"
+    if not os.path.exists(history_dir):
+        os.makedirs(history_dir)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"{timestamp}_conversation.txt"
+    file_path = os.path.join(history_dir, filename)
+
+    with open(file_path, "w") as f:
+        for role, content in conversation:
+            f.write(f"{role.capitalize()}: {content}\n")
 
 
-def has_qa_history():
-    return len(qa_history) > 0
+def count_tokens(messages, model=MODEL):
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        return count_tokens(messages, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        return count_tokens(messages, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
 
 
-def get_qa_history_formatted():
-    return '\n'.join(qa_history)
-
-
-def previous_qa_history_length_ok():
-    qas_formatted = get_qa_history_formatted()
-    num_tokens = get_num_tokens(qas_formatted)
-    return num_tokens <= MAX_HISTORY_TOKENS
-
-
-def trim_qas():
-    while not previous_qa_history_length_ok():
-        qa_history.pop()
-        trim_qas()
+def trim_conversation(prompt, max_tokens=MAX_TOKENS):
+    while count_tokens(prompt) > max_tokens:
+        if len(prompt) <= 1:
+            break
+        prompt.pop(1)  # Remove the second message in the prompt (first non-system message)
+    return prompt
 
 
 def do_request(prompt):
+    prompt = trim_conversation(prompt)
+
     print_magenta("\nThinking...")
     try:
         response = openai.ChatCompletion.create(
@@ -85,12 +113,12 @@ def build_prompt(conversation, topic=None):
     parts = []
     directives = []
     directives.append(
-        'You are an AI research assistant that answers questions factually correct, the answers should lead to curiosity. Elaborate on the answers as much as possible. Here are your directives:')
+        'You are a research assistant that answers questions factually correct, the answers should lead to curiosity. Elaborate on the answers as much as possible. Here are your directives:')
     directives.append('1. Answer the user question.')
     if topic:
         directives.append(f'2. Come up with a topic that suits the latest 3 user questions, the topic should be short and descriptive.')
     directives.append(
-        f'{"3" if topic else "2"}. Create {"6" if topic else "5"} follow-up questions for your current answer.{f" Try and stay as close to the topic {topic_formatted} as possible, unless the user clearly changes interest. The 6th follow-up question should derive from the topic a bit, while still being related to the last user message. " if topic else ""}.')
+        f'{"3" if topic else "2"}. Create {"6" if topic else "5"} follow-up questions for your current answer. The questions should be directed to the assistant.{f" Try and stay as close to the topic {topic_formatted} as possible, unless the user clearly changes interest. The 6th follow-up question should derive from the topic a bit, while still being related to the last user message. " if topic else ""}.')
     parts.append('\n'.join(directives))
     if len(conversation):
         parts.append(
@@ -104,7 +132,6 @@ def build_prompt(conversation, topic=None):
     for msg in conversation:
         role, content = msg
         prompt.append({"role": role, "content": content})
-    # prompt.append({"role": "user", "content": question})
 
     debug_log('\nPrompt: {}'.format(json.dumps(prompt, indent=4)))
 
@@ -142,8 +169,6 @@ def ask(question, conversation, topic=None):
 
     if answer:
         print_green(f"\n{answer}")
-        qa_history.append(f'Question: {question}\nAnswer: {answer}')
-        trim_qas()
         success = True
 
     if options:
@@ -158,6 +183,11 @@ def ask(question, conversation, topic=None):
 def ask_and_append(conversation, question, topic=None):
     success, answer, updated_topic, options = ask(question, conversation, topic)
     topic = updated_topic if updated_topic else topic
+
+    if success:
+        conversation.append(("assistant", answer))
+        save_conversation_history(conversation)
+
     return success, answer, topic, options
 
 
@@ -177,7 +207,7 @@ def main():
                 user_input = input(
                     '\nChoose a follow-up question by entering the corresponding number (1-5) or type a custom question (type exit to quit):\n')
 
-                if user_input.isdigit() and 1 <= int(user_input) <= 6:
+                if user_input.isdigit() and 1 <= int(user_input) <= len(options):
                     question = options[int(user_input) - 1]
                 else:
                     question = user_input
@@ -194,12 +224,10 @@ def main():
             print("\nSorry, I couldn't find an answer to your question. Try another one.\n")
             continue
 
-        conversation.append(("assistant", answer))
-
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         print('\n\nBye-bye!')
-        quit()
+        sys.exit()
